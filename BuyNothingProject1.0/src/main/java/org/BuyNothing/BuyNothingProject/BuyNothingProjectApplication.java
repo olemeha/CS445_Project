cf6d6a00a8a1ed401ba25a1709c7da4657415628
olemeha <77707992+olemeha@users.noreply.github.com>
2022-03-15 17:15:42 -0500

    create a maven project with spring boot

---
+++ BuyNothingProject1.0/src/main/java/org/BuyNothing/BuyNothingProject/BuyNothingProjectApplication.java
@@ -0,0 +1,13 @@
+package org.BuyNothing.BuyNothingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyNothingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyNothingProjectApplication.class, args);
+	}
+
+}
